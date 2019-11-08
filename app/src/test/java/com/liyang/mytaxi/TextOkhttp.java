1fdfb64985f5891b69ab3e9b314574117e4db349
Leeyung <763547760@qq.com>
2019-11-08 15:59:06 +0800

    text okhttp

---
+++ app/src/test/java/com/liyang/mytaxi/TextOkhttp.java
@@ -0,0 +1,8 @@
+package com.liyang.mytaxi;
+
+/**
+ * Created by TodayFu Lee on 2019/11/8.
+ */
+
+public class TextOkhttp {
+}
---
+++ app/src/test/java/com/liyang/mytaxi/TextOkhttpTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by TodayFu Lee on 2019/11/8.
+ */
+public class TextOkhttpTest {
+
+}
